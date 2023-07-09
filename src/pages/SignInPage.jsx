6972577c8885efabd826e3b034003c8ef9263436
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-07-09 16:19:55 -0300

    Implemented SignIn functionality.

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,15 +1,36 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignInPage() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const signIn = (e) => {
+    e.preventDefault();
+    const URL = `${import.meta.env.VITE_API_URL}/signin`;
+    const request = axios.post(URL, { 'email': email, 'password': password });
+    request
+      .then((res) => {
+        localStorage.setItem('name', res.data.name);
+        localStorage.setItem('token', res.data.token);
+        navigate('/home');
+      })
+      .catch((error) => { alert(error.response.data) });
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signIn}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
-        <button>Entrar</button>
+        <input placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} data-test="email" />
+        <input placeholder="Senha" type="password" autoComplete="new-password" value={password} onChange={e => setPassword(e.target.value)} data-test="password" />
+        <button data-test="sign-in-submit">Entrar</button>
       </form>
 
       <Link to="/cadastro">
@@ -17,6 +38,7 @@ export default function SignInPage() {
       </Link>
     </SingInContainer>
   )
+
 }
 
 const SingInContainer = styled.section`
